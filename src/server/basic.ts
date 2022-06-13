53b1e973845e1bc827bbbdfffd63a65ec90baa3d
Pon <perterpon.wang@bytedance.com>
2022-06-13 17:56:01 +0800

    fix issue

--- src/server/basic.ts
+++ src/server/basic.ts
@@ -7,12 +7,7 @@ import * as mysql from '../mysql';
 import { TPTServer } from '../types';
 
 export async function getCurrentServerInfo(): Promise<TPTServer> {
-  // const macs = await macAddress.all();
-  const macs = {
-    name: {
-      mac: '00:11:32:f3:be:33'
-    }
-  };
+  const macs = await macAddress.all();
   const servers: TPTServer[] = await mysql.getAllServers();
   for (const interfaceName in macs) {
     const { mac } = macs[interfaceName];
