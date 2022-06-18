4fe4729a5c0d6bf2d41e8e48ab211ff2d7a4acfc
Pon <perterpon.wang@bytedance.com>
2022-06-18 15:35:46 +0800

    display puppeteer display

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -32,7 +32,7 @@ export async function init(): Promise<void> {
   const userDataDir: string = await getUserDataDir();
   await mkdirp(userDataDir);
   browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     executablePath: null,
     ignoreDefaultArgs: [],
     args: [
